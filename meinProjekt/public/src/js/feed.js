00d21b304513db34169d2a4204bfc7eda914c5ab
angeliallieu <angeli.allieu@googlemail.com>
2023-09-24 20:40:29 +0200

    Kamera anzeigen geht

--- meinProjekt/public/src/js/feed.js
+++ meinProjekt/public/src/js/feed.js
@@ -1,7 +1,7 @@
-let createMomentButton = document.querySelector('#create-moment-button');
-//let captureArea = document.querySelector('#create-post');
-// let closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
-let capturedMomentsArea = document.querySelector('#captured-moments');
+let shareImageButton = document.querySelector('#share-image-button');
+let createPostArea = document.querySelector('#create-post');
+let closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+let sharedMomentsArea = document.querySelector('#shared-moments');
 let form = document.querySelector('form');
 let sdescrInput = document.querySelector('#sdescr');
 let notesInput = document.querySelector('#notes');
@@ -12,25 +12,65 @@ let captureButton = document.querySelector('#capture-btn');
 let imagePicker = document.querySelector('#image-picker');
 let imagePickerArea = document.querySelector('#pick-image');
 let file = null;
-let titleValue = '';
+let sdescrValue = '';
+let notesValue = '';
 let locationValue = '';
 let imageURI = '';
 
 function initializeMedia() {
+    if(!('mediaDevices' in navigator)) {
+        navigator.mediaDevices = {};
+    }
 
+    if(!('getUserMedia' in navigator.mediaDevices)) {
+        navigator.mediaDevices.getUserMedia = function(constraints) {
+            let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+            if(!getUserMedia) {
+                return Promise.reject(new Error('getUserMedia is not implemented'));
+            }
+
+            return new Promise( (resolve, reject) => {
+                getUserMedia.call(navigator, constraints, resolve, reject);
+            })
+        }
+    }
+
+    navigator.mediaDevices.getUserMedia({video: true})
+    .then( stream => {
+        videoPlayer.srcObject = stream;
+        videoPlayer.style.display = 'block';
+    })
+    .catch( err => {
+        imagePickerArea.style.display = 'block';
+    });
 }
 
-// function openCapturePostModal() {
-//   captureArea.style.display = 'block';
-// }
+function openCreatePostModal() {
+    setTimeout( () => {
+        createPostArea.style.transform = 'translateY(0)';
+    }, 1);
+    initializeMedia();
+}
 
-// function closeCreatePostModal() {
-//   createPostArea.style.display = 'none';
-// }
+function closeCreatePostModal() {
+    imagePickerArea.style.display = 'none';
+    videoPlayer.style.display = 'none';
+    canvasElement.style.display = 'none';
+    if(videoPlayer.srcObject) {
+        videoPlayer.srcObject.getVideoTracks().forEach( track => track.stop());
+    }
+    setTimeout( () => {
+        createPostArea.style.transform = 'translateY(100vH)';
+    }, 1);
+}
 
-createMomentButton.addEventListener('click', openCreatePostModal);
 
-// closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+
 
 function createCard(card) {
     let cardWrapper = document.createElement('div');
@@ -65,7 +105,6 @@ function createCard(card) {
   }
 
   
-let networkDataReceived = false;
 
 fetch('http://localhost:4000/posts')
     .then((res) => {
@@ -135,13 +174,41 @@ form.addEventListener('submit', event => {
         return;
     }
 
-    // closeCreatePostModal();
+    closeCreatePostModal();
 
     sdescrValue = sdescrInput.value;
     notesValue = notesInput.value;
     locationValue = locationInput.value;
 
     sendDataToBackend();
+});
+
+
+imagePicker.addEventListener('change', event => {
+    file = event.target.files[0];
+});
+
+captureButton.addEventListener('click', event => {
+    event.preventDefault(); // nicht absenden und neu laden
+    canvasElement.style.display = 'block';
+    videoPlayer.style.display = 'none';
+    captureButton.style.display = 'none';
+    let context = canvasElement.getContext('2d');
+    context.drawImage(videoPlayer, 0, 0, canvas.width, videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width));
+    videoPlayer.srcObject.getVideoTracks().forEach( track => {
+        track.stop();
+    })
+    imageURI = canvas.toDataURL("image/jpg");
+    // console.log('imageURI', imageURI)       // base64-String des Bildes
+    
+    fetch(imageURI)
+    .then(res => {
+        return res.blob()
+    })
+    .then(blob => {
+        file = new File([blob], "myFile.jpg", { type: "image/jpg" })
+        console.log('file', file)
+    })
 });
 
 
